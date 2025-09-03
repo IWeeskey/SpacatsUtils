314f7da6916841b8120dffea9849d10522973172
IvanKurdakov <kis.flack.more.more@gmail.com>
2025-09-03 17:28:32 +0300

    - code cleanup- added iterations test scene

---
+++ Examples/Spacats Utils Examples/MonoTween/Scripts/IterationsTest.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+namespace Spacats.Utils
+{
+    public class IterationsTest : GUIButtons
+    {
+        [Header("MonoTweenTest Settings")]
+        private int _oneMil = 1000000;
+
+        private void Awake()
+        {
+            Application.targetFrameRate = 60;
+        }
+
+        private void Update()
+        {
+
+        }
+
+
+        protected override string GetButtonLabel(int index)
+        {
+            switch (index)
+            {
+                default: return base.GetButtonLabel(index);
+                case 0:
+                    if (!GUILogViewer.Instance.LoggingEnabled) return "Logging Disabled";
+                    return GUILogViewer.Instance.IsOpened ? "Hide Log" : "Show Log";
+                case 1: return "Start 1mil";
+                case 2: return "Start 100k";
+                case 3: return "Start 10k";
+            }
+        }
+
+        protected override void OnButtonClick(int index)
+        {
+            switch (index)
+            {
+                default: base.OnButtonClick(index); break;
+                case 0: SwitchShowHideLog(); break;
+                case 1: LaunchIterations(_oneMil); break;
+                case 2: LaunchIterations(_oneMil/10); break;
+                case 3: LaunchIterations(_oneMil / 100); break;
+            }
+        }
+
+        private void SwitchShowHideLog()
+        {
+            if (GUILogViewer.Instance.IsOpened) GUILogViewer.Instance.CloseLog();
+            else GUILogViewer.Instance.OpenLog();
+        }
+
+        private void LaunchIterations(int count)
+        {
+            TimeTracker.Start("IterationsTest");
+
+            int counter = 0;
+            for (int i = 0; i < count; i++)
+            {
+                counter++;
+            }
+            Debug.Log("Count: " + counter);
+            TimeTracker.Finish("IterationsTest");
+        }
+
+    }
+}
